0ba3783455ec6508f806155b6ee848de8ce14389
Bernard Anderson <bernard.anderson@gmail.com>
2016-10-13 22:23:12 -0500

    The student view and viewcontroller were added. The student view was added along with the view controller.  The table was built and styled using bootstrap.

--- RepoQuiz/Controllers/StudentController.cs
+++ RepoQuiz/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepoQuiz.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace RepoQuiz.Controllers
         // GET: Student
         public ActionResult Index()
         {
+            ViewBag.StudentList = new StudentRepository().GetAllStudents();
             return View();
         }
 
---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RepoQuiz.Models;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepository
+    {
+        public StudentContext Context { get; set; }
+
+        public StudentRepository()
+        {
+            Context = new StudentContext();
+        }
+
+        public StudentRepository(StudentContext _context)
+        {
+            Context = _context;
+        }
+
+        // Gets the Student table, adds them to the Student "object" and adds each "object" to a List<>
+        virtual public List<Student> GetAllStudents()
+        {
+            var StudentTable = Context.Students;
+
+            List<Student> ListOfStudents = new List<Student>();
+
+            foreach (var student in StudentTable)
+            {
+                ListOfStudents.Add( new Student()
+                {
+                    StudentID = student.StudentID,
+                    FirstName = student.FirstName,
+                    LastName = student.LastName,
+                    Major = student.Major
+                });
+            }
+            return ListOfStudents;
+        }
+
+
+
+    }
+}
